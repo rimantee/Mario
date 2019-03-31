731f40504ba6de4e3f1f1ba7eb62307c534ba98e
rimantee <35688293+rimantee@users.noreply.github.com>
2019-03-31 11:50:21 +0300

    Add Finish timeline

---
+++ Assets/FlagScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class FlagScript : MonoBehaviour
+{
+    public PlayableDirector finish;
+
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public float moveSpeed;
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+            finish.Play();
+        }
+    }
+}
